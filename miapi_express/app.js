a3986d7a4873d67071ab2ca103e95b4b7cf8aa84
guillesaiz <guillermosaiz1990@gmail.com>
2023-02-04 20:01:47 +0100

    Borrado prueba

--- miapi_express/app.js
+++ miapi_express/app.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require('express'); // IMP
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors')
+/* const cors = require('cors') */
 
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index'); // IMP -> Carpeta routes
+var usersRouter = require('./routes/users'); // IMP -> Carpeta routes - se puede borrar. Creo que la de arriba también, solo me define una RUTA. 
 
 var app = express();
 
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter); // IMP RUTAS
+app.use('/users', usersRouter); // IMP
 
 module.exports = app;
 
